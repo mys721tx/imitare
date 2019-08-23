151a685fab131a7caf4fbac337951c022ef9fe0f
Yishen Miao <mys721tx@gmail.com>
2019-08-23 16:02:34 -0400

    Using less expensive default
    Uses less expensive default for file size.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use clap::{App, Arg};
+use rand::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use clap::{App, Arg};
-use rand::prelude::*;
 
 fn main() {
     let mut rng = StdRng::from_entropy();
@@ -16,7 +16,7 @@ fn main() {
                 .short("o")
                 .long("out")
                 .value_name("FILE")
-                .help("Sets the name of the output file.")
+                .help("Sets the name of the output file. Defaults to \"output.txt\".")
                 .takes_value(true),
         )
         .arg(
@@ -24,7 +24,7 @@ fn main() {
                 .short("s")
                 .long("size")
                 .value_name("SIZE")
-                .help("Sets the size of the output in byte.")
+                .help("Sets the size of the output in byte. Defaults to 4096.")
                 .takes_value(true),
         )
         .get_matches();
@@ -32,9 +32,8 @@ fn main() {
     let file = matches.value_of("output").unwrap_or("output.txt");
     let size = matches
         .value_of("size")
-        .unwrap_or("4096")
-        .parse::<u64>()
-        .unwrap();
+        .and_then(|x| x.parse::<u64>().ok())
+        .unwrap_or(4096);
     let mut buffer = File::create(file).unwrap();
 
     let r = &mut rng as &mut dyn RngCore;
