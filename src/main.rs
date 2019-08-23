35b7e79b94779734c9f25dd26fd76430849a4f96
Yishen Miao <mys721tx@gmail.com>
2019-08-22 23:46:50 -0400

    Adding command-line parameters
    Adds command-line parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::fs::File;
+use clap::{App, Arg};
 use rand::prelude::*;
 
 fn main() {
-    let mut buffer = File::create("foo.txt").unwrap();
     let mut rng = StdRng::from_entropy();
 
-    let r = &mut rng as &mut dyn RngCore;
+    let matches = App::new("imitare")
+        .version("0.1")
+        .author("Yishen Miao <mys721tx@gmail.com>")
+        .about("A fake file generator")
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("out")
+                .value_name("FILE")
+                .help("Sets the name of the output file.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("size")
+                .short("s")
+                .long("size")
+                .value_name("SIZE")
+                .help("Sets the size of the output in byte.")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let size = 854_235;
+    let file = matches.value_of("output").unwrap_or("output.txt");
+    let size = matches
+        .value_of("size")
+        .unwrap_or("4096")
+        .parse::<u64>()
+        .unwrap();
+    let mut buffer = File::create(file).unwrap();
+
+    let r = &mut rng as &mut dyn RngCore;
 
     io::copy(&mut r.take(size), &mut buffer).unwrap();
 }
